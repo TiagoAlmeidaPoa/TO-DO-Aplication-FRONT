1dc4d966c3390aa1d4027b966234b821b82e5ced
Tiago Almeida <tiago.almeida.poa@gmail.com>
2023-07-11 20:40:51 -0300

    feat: adjusted angular.json and app.modules.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { AppRoutingModule } from "./app-routing.module";
@@ -27,6 +28,7 @@ import { MatIconModule } from "@angular/material/icon";
     MatCardModule,
     MatButtonModule,
     MatIconModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent],
